0ede775c2871034b49dbd45d3e0877e5586500ae
Paul Goodchild <paul@icontrolwp.com>
2017-12-30 21:27:57 +0000

    add to the bridge interface

--- src/Reconciliation/Bridge/BridgeInterface.php
+++ src/Reconciliation/Bridge/BridgeInterface.php
@@ -2,39 +2,50 @@
 
 namespace FernleafSystems\Integrations\Freeagent\Reconciliation\Bridge;
 
-use FernleafSystems\ApiWrappers\Freeagent\Entities\Contacts\ContactVO;
-use FernleafSystems\ApiWrappers\Freeagent\Entities\Invoices\InvoiceVO;
-use FernleafSystems\Integrations\Freeagent\DataWrapper\ChargeVO;
+use FernleafSystems\ApiWrappers\Freeagent\Entities;
+use FernleafSystems\Integrations\Freeagent\DataWrapper;
 
 interface BridgeInterface {
 
 	/**
-	 * @param ChargeVO $oCharge
-	 * @param bool     $bUpdateOnly
-	 * @return ContactVO
+	 * @param string $sTxnID
+	 * @return DataWrapper\ChargeVO
+	 */
+	public function buildChargeFromTransaction( $sTxnID );
+
+	/**
+	 * @param string $sPayoutId
+	 * @return DataWrapper\PayoutVO
+	 */
+	public function buildPayoutFromId( $sPayoutId );
+
+	/**
+	 * @param DataWrapper\ChargeVO $oCharge
+	 * @param bool                 $bUpdateOnly
+	 * @return Entities\Contacts\ContactVO
 	 */
 	public function createFreeagentContact( $oCharge, $bUpdateOnly = false );
 
 	/**
-	 * @param ChargeVO $oCharge
-	 * @return InvoiceVO
+	 * @param DataWrapper\ChargeVO $oCharge
+	 * @return Entities\Invoices\InvoiceVO
 	 */
 	public function createFreeagentInvoice( $oCharge );
 
 	/**
-	 * @param ChargeVO $oCharge
+	 * @param DataWrapper\ChargeVO $oCharge
 	 * @return int
 	 */
 	public function getFreeagentContactId( $oCharge );
 
 	/**
-	 * @param ChargeVO $oCharge
+	 * @param DataWrapper\ChargeVO $oCharge
 	 * @return int
 	 */
 	public function getFreeagentInvoiceId( $oCharge );
 
 	/**
-	 * @param ChargeVO $oCharge
+	 * @param DataWrapper\ChargeVO $oCharge
 	 * @return bool
 	 */
 	public function verifyInternalPaymentLink( $oCharge );
