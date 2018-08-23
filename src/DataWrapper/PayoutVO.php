7d6b4fa9ffae56dce43b245801b802f6a21d7434
Paul G <paul@icontrolwp.com>
2018-08-23 14:46:31 +0100

    support in the payout for refunds

--- src/DataWrapper/PayoutVO.php
+++ src/DataWrapper/PayoutVO.php
@@ -13,6 +13,20 @@ class PayoutVO {
 	use StdClassAdapter;
 
 	/**
+	 * @param ChargeVO|RefundVO $oItem
+	 * @return $this
+	 */
+	public function addItem( $oItem ) {
+		if ( $oItem instanceof ChargeVO ) {
+			$this->addCharge( $oItem );
+		}
+		else if ( $oItem instanceof RefundVO ) {
+			$this->addRefund( $oItem );
+		}
+		return $this;
+	}
+
+	/**
 	 * @param ChargeVO $oCharge
 	 * @return $this
 	 */
@@ -26,11 +40,32 @@ class PayoutVO {
 	}
 
 	/**
+	 * @param RefundVO $oRefund
+	 * @return $this
+	 */
+	public function addRefund( $oRefund ) {
+		if ( !$this->hasRefund( $oRefund ) ) {
+			$aR = $this->getRefunds();
+			$aR[] = $oRefund;
+			$this->setRefunds( $aR );
+		}
+		return $this;
+	}
+
+	/**
 	 * @return ChargeVO[]
 	 */
 	public function getCharges() {
-		$aC = $this->getArrayParam( 'charges' );
-		return is_array( $aC ) ? $aC : array();
+		$a = $this->getArrayParam( 'charges' );
+		return is_array( $a ) ? $a : array();
+	}
+
+	/**
+	 * @return RefundVO[]
+	 */
+	public function getRefunds() {
+		$a = $this->getArrayParam( 'refunds' );
+		return is_array( $a ) ? $a : array();
 	}
 
 	/**
@@ -124,11 +159,34 @@ class PayoutVO {
 	}
 
 	/**
+	 * @param RefundVO $oRefund
+	 * @return bool
+	 */
+	public function hasRefund( $oRefund ) {
+		$bExists = false;
+		foreach ( $this->getRefunds() as $oR ) {
+			if ( $oR->getId() == $oRefund->getId() ) {
+				$bExists = true;
+				break;
+			}
+		}
+		return $bExists;
+	}
+
+	/**
 	 * @param ChargeVO[] $mVal
 	 * @return $this
 	 */
 	public function setCharges( $mVal ) {
 		return $this->setParam( 'charges', $mVal );
+	}
+
+	/**
+	 * @param RefundVO[] $aR
+	 * @return $this
+	 */
+	public function setRefunds( $aR ) {
+		return $this->setParam( 'refunds', $aR );
 	}
 
 	/**
