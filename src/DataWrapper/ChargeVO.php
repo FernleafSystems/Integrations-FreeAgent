514f5985c1a5972620fa7b455db3518d7f01f40c
Paul G <paul@icontrolwp.com>
2023-07-18 09:03:45 +0100

    adjust how certain fields are handled in ChargeVO

--- src/DataWrapper/ChargeVO.php
+++ src/DataWrapper/ChargeVO.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare( strict_types=1 );
 
 namespace FernleafSystems\Integrations\Freeagent\DataWrapper;
 
@@ -22,174 +22,45 @@ class ChargeVO extends BaseTxnVO {
 	public function __get( string $key ) {
 		$val = parent::__get( $key );
 		switch ( $key ) {
-			case 'is_vatmoss':
-				if ( is_null( $val ) ) {
-					$val = is_null( $val ) ? $this->ec_status === Constants::VAT_STATUS_EC_MOSS : (bool)$val;
+			case 'payment_terms':
+				if ( $val === null ) {
+					$val = 10;
 				}
 				break;
-
+			case 'is_vatmoss':
+				if ( $val === null ) {
+					$val = $this->ec_status === Constants::VAT_STATUS_EC_MOSS;
+				}
+				break;
+			case 'item_quantity':
+				if ( $val === null ) {
+					$val = 1;
+				}
+				break;
 			case 'ec_status':
-				if ( is_null( $val ) ) {
+				if ( $val === null ) {
 					$val = Constants::VAT_STATUS_UK_NON_EC;
 				}
 				break;
-
 			case 'item_taxrate':
+				// out of 100%
 				if ( $val > 0 && $val < 1 ) {
 					$val *= 100;
 				}
-				$val = (int)abs( round( $val ) );
+				$val = (int)\abs( \round( $val ) );
 				break;
-
+			case 'item_type':
+				if ( empty( $val ) ) {
+					$val = 'Years';
+				}
+				$singular = \strtolower( \rtrim( $val, 's' ) );
+				if ( \in_array( $singular, [ 'hour', 'day', 'week', 'month', 'year' ] ) ) {
+					$val = \ucfirst( \strtolower( \rtrim( $val, 's' ).'s' ) );
+				}
+				break;
 			default:
 				break;
 		}
 		return $val;
-	}
-
-	/**
-	 * @return string
-	 * @deprecated
-	 */
-	public function getCountry() {
-		return $this->country;
-	}
-
-	/**
-	 * @return string
-	 * @deprecated
-	 */
-	public function getGateway() {
-		return $this->gateway;
-	}
-
-	/**
-	 * @return string
-	 * @deprecated
-	 */
-	public function getItemName() {
-		return $this->item_name;
-	}
-
-	public function getItemQuantity() :int {
-		return (int)( $this->item_quantity ?? 1 );
-	}
-
-	public function getItemSubtotal() :float {
-		return (float)( $this->item_subtotal ?? 1 );
-	}
-
-	/**
-	 * Out of 100%
-	 * @return float|int
-	 */
-	public function getItemTaxRate() :int {
-		$val = (float)$this->item_taxrate;
-		if ( $val > 0 && $val < 1 ) {
-			$val *= 100;
-		}
-		return (int)abs( round( $val ) );
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getItemPeriodType() {
-		return $this->item_type ?? 'Years';
-	}
-
-	public function getLocalPaymentId() {
-		return $this->local_payment_id ?? 0;
-	}
-
-	public function getPaymentTerms() :int {
-		return (int)$this->payment_terms ?? 5;
-	}
-
-	/**
-	 * @deprecated 2.1.0
-	 */
-	public function isEuVatMoss() :bool {
-		return (bool)$this->is_vatmoss ?? false;
-	}
-
-	/**
-	 * @deprecated 2.1.0
-	 */
-	public function setGateway( string $value ) :self {
-		$this->gateway = $value;
-		return $this;
-	}
-
-	/**
-	 * @deprecated 2.1.0
-	 */
-	public function setIsEuVatMoss( bool $isMoss ) :self {
-		$this->is_vatmoss = $isMoss;
-		return $this;
-	}
-
-	/**
-	 * @param string $value
-	 * @return $this
-	 */
-	public function setItemName( $value ) :self {
-		$this->item_name = $value;
-		return $this;
-	}
-
-	/**
-	 * @param int $value
-	 * @return $this
-	 */
-	public function setItemQuantity( $value ) :self {
-		$this->item_quantity = $value;
-		return $this;
-	}
-
-	/**
-	 * @param float $value
-	 * @return $this
-	 */
-	public function setItemSubtotal( $value ) :self {
-		$this->item_subtotal = $value;
-		return $this;
-	}
-
-	/**
-	 * @param float $value
-	 * @return $this
-	 */
-	public function setItemTaxRate( $value ) :self {
-		$this->item_taxrate = $value;
-		return $this;
-	}
-
-	/**
-	 * @param string $periodType
-	 * @return $this
-	 */
-	public function setItemPeriodType( string $periodType ) :self {
-		$this->item_type = $periodType;
-		return $this;
-	}
-
-	/**
-	 * @param mixed $value
-	 * @return $this
-	 */
-	public function setLocalPaymentId( $value ) :self {
-		$this->local_payment_id = $value;
-		return $this;
-	}
-
-	/**
-	 * @param int $terms
-	 * @return $this
-	 * @deprecated
-	 */
-	public function setPaymentTerms( $terms ) {
-		$this->payment_terms = $terms;
-		return $this;
 	}
 }
